00e944e83b677044943dc6b1df644898a85dbaf0
irockafe <isaac.rockafellow@gmail.com>
2015-10-10 16:58:46 -0400

    v0.1, Effectively parses HMDB with an input from a metlin CSV

--- parse_metlin_csv.py
+++ parse_metlin_csv.py
@@ -2,209 +2,140 @@ from bs4 import BeautifulSoup
 import csv
 from sys import argv
 import pandas
+from lxml import etree
+from collections import defaultdict
 #BeautifulSoup also requires libxml-dev and libxslt-dev packages!
 
 '''
 This code takes the csv file downloaded from at 
-METLIN batch search. It will search HMDB for METLIN hits
-and add information, like if the metabolite is from urine or 
+METLIN batch search. It will search a downloaded HMDB database
+(xml-format) for the masses identified by METLIN and add 
+HMDB information, like if the metabolite is from urine or 
 feces, to a new csv file
 
-Metlin does not output masses that where nothing was found.
-So, users should add their input masses and I can infer that
-nothing was found if the METLIN csv has nothing in it for that mass
+Metlin does not output masses where nothing was found.
 '''
 #You have to point python to your metlin csv in the
 #command line
 #script, metlin_csv = argv
 
 #Path to csv file
-#bigger toy metlin_csv = '/home/irockafe/Documents/MIT/Fall_2015/Alm_Lab/METLIN_parse_hmdb_search/metlin_toy_data.csv'
-metlin_csv = '/home/irockafe/Documents/MIT/Fall_2015/Alm_Lab/METLIN_parse_hmdb_search/smaller_toy_data.csv'
+metlin_csv = '/home/irockafe/Documents/MIT/Fall_2015/Alm_Lab/METLIN_parse_hmdb_search/negativePeaks_sept28_METLIN.csv'
+#metlin_csv = '/home/irockafe/Documents/MIT/Fall_2015/Alm_Lab/METLIN_parse_hmdb_search/metlin_toy_data.csv'
+#metlin_csv = '/home/irockafe/Documents/MIT/Fall_2015/Alm_Lab/METLIN_parse_hmdb_search/smaller_toy_data.csv'
 
-hmdb_all_metabolites = '/home/irockafe/Documents/MIT/Fall_2015/Alm_Lab/METLIN_parse_hmdb_search/hmdb_metabolites/hmdb_metabolites.xml'
-toy_hmdb_all_metabolites = '/home/irockafe/Documents/MIT/Fall_2015/Alm_Lab/METLIN_parse_hmdb_search/toy_hmdb/toy_hmdb.xml'
+hmdb_all_metabolites = '/home/irockafe/Documents/MIT/Fall_2015/Alm_Lab/METLIN_parse_hmdb_search/hmdb_metabolites/removed_xml_hmdb_metabolites.xml'
+toy_hmdb_all_metabolites = '/home/irockafe/Documents/MIT/Fall_2015/Alm_Lab/METLIN_parse_hmdb_search/toy_hmdb/removed_xml_toy_hmdb.xml'
 
 hmdb_directory = '/home/irockafe/Documents/MIT/Fall_2015/Alm_Lab/METLIN_parse_hmdb_search/hmdb_metabolites/'
 toy_hmdb_directory = '/home/irockafe/Documents/MIT/Fall_2015/Alm_Lab/METLIN_parse_hmdb_search/toy_hmdb/'
-output_filename = '/home/irockafe/Documents/MIT/Fall_2015/Alm_Lab/METLIN_parse_hmdb_search/toy_hmdb/HOLYSHIT_IT_WORKED.csv'
-#import csv to pandas, with first row as the header
-metlin_data = pandas.read_csv(metlin_csv, header=0)
-masses = metlin_data['mass']
-#Count number of repeat input mass values - the number of isomers
-isomers = metlin_data['inputmass'].value_counts()
 
-#These are the masses you should search HMDB for
-#We're not searching for individual compounds, instead
-#we want to find anything that can match our mass
-#masses_to_search = metlin_data['mass'].value_counts()
-masses = metlin_data['mass']
-#mol_ids = metlin_data['molid']
-#adduct_masses_df = metlin_data[['inputmass', 'mass']]
-#Convert dataframe into dictionary object
-#molid_masses_dict = molid_masses_df.set_index('inputmass').T.to_dict('list')
+output_filename = '/home/irockafe/Documents/MIT/Fall_2015/Alm_Lab/METLIN_parse_hmdb_search/toy_hmdb/all_mass_tests.csv'
 
-toy_masses = [102.031694058, 74.08439833]
-metabolites = BeautifulSoup(open(hmdb_all_metabolites), "lxml").find_all('metabolite')
+
+#metabolites = BeautifulSoup(open(hmdb_all_metabolites), "lxml").find_all('metabolite')
+
 #save memory by not opening two giant files
 #hmdb_soup = BeautifulSoup(open(hmdb_all_metabolites), "lxml")
 #metabolites = hmdb_soup.find_all('metabolite')
 
-def search_HMDB(masses, metabolites):
+def get_data_from_hmdb(xml_file, masses, mass_deviation_tolerance):
 	'''
-	INPUT - a dictionary of {mass: molid} of the masses METLIN identified as 
-	compounds and their molids, and an xml tree of metabolites
+	INPUT - path to an xml database file and a list of masses you
+			want to search
+	FUNCTION - Find entries in hmdb that match our masses,
+				get relevant info - name, biofluid location
 
-	TODO: Modify code to work on a single metabolite
+	OUTPUT - {Mass1: {HMDB_ID-1:{Urine: x, Feces: 0, Others: 'string'}}}
+	'''
 
-	FUNCTION - parse a downloaded HMDB directory 
-	OUTPUT - a dictionary of masses and HMDB ID numbers that match
-	this mass ex: {mass: [HMDB_ID_1, HMDB_ID_2]}
-	 These will be used to access individual HMDB_ID 
-	files to make searching faster (??)
-	'''
-	print 'Beginning to search HMDB'
-	hmdb_matches = {}
-	for metabolite in metabolites:
-		MW = float(metabolite.monisotopic_moleculate_weight.contents[0])
-		#TODO - may need to not do exact matching, but define a 0.1ppm 
-		#threshold to determine if MW is one of our target masses.
-		if MW in masses:
-			print 'Found a match for mass %s: %s!' % (MW, metabolite)
-			#unicode string
-			hmdb_id = metabolite.accession.contents[0]
-			#Add {MW:id} to output dictionary
-			if MW in hmdb_matches:
-				hmdb_matches[MW].append(hmdb_id)
-			else:
-				hmdb_matches[MW] = [hmdb_id]
+	log_file = open('log_parse_hmdb.txt', 'w')
+	output_nested_dict = defaultdict(dict)
+	#Go through each metabolite, one at a time
+	xml_tree = etree.iterparse(xml_file, tag='metabolite')
+	count = 0
+	for event, element in xml_tree:
+		count += 1
+		if count % 5000 == 0:
+			print 'Working on compound #%s' % count
+		#print '%s -- %s\n\n' % (event, element.findtext('monisotopic_moleculate_weight'), )
+		#Get the MW
+		monisotopic_tag_text = element.findtext('monisotopic_moleculate_weight')
+		'''
+		print monisotopic_tag_text
+		print monisotopic_tag_text == ''
+		'''
+		#If no MW listed, skip over that metabolite and write to log 
+		#file that you did so.
+		if monisotopic_tag_text == '':
+			compound_name = element.findtext('name')
+			hmdb_id = element.findtext('accession') 
+			log_file.write('Skipped over %s, %s, because no MW was listed\n' % (hmdb_id, compound_name))
+			#Go to next iteration of for loop
+			continue
 
-	return hmdb_matches
+		MW = float(element.findtext('monisotopic_moleculate_weight'))
+		hmdb_id = element.findtext('accession')
 
-def get_hmdb_info(hmdb_identified):
-	'''
-	INPUT - dictionary of {mass1:[HMDB_ID_1, HMDB_ID_2, ...], 
-						   mass2:...}
-	FUNCTION - Open the correct HMDB metabolite's file and get
-		information that you want from it. i.e. biofluid, 
-		disease type, etc.
-	OUTPUT - {Mass_1: {HMDB_ID_1: {Urine: 0, Feces: 0, Others}, 
-			  		   HMDB_ID_2: ...},
-			  Mass_2: ...
-			  }
-	'''
-	print 'Finished Identifying Potential Compounds.\n Moving on to gathering compound information.'
-	output = {}
-	for mass in hmdb_identified.keys():
-		output[mass] = {}
-		#print mass
-		for entry in hmdb_identified[mass]:
-			#print entry
-			with open(hmdb_directory+entry+'.xml') as metabolite:
-				#Get biofluid status
-				metabolite_soup = BeautifulSoup(metabolite)
-				biofluid_info = which_biofluids(metabolite_soup)
-			output[mass][entry] = biofluid_info
-	return output
+		for mass in masses:
+			'''
+			if hmdb_id == 'HMDB11635':
+				print '\n',hmdb_id
+				print mass
+				print type(masses[0])
+				print MW
+				print type(MW)
+				print abs(MW-mass) < mass_deviation_tolerance, '\n'
+			'''
+			if abs(MW - mass) < mass_deviation_tolerance:
 
-def which_biofluids(hmdb_metabolite):
+				hmdb_id = element.findtext('accession')
+				
+				compound_name = element.findtext('name')
+				#print 'Found Mass %s, hmdb: %s, name: %s'% (MW, hmdb_id, compound_name)
+
+				#Get a list of biofluid locations
+				biofluid_locations =  element.find('biofluid_locations').findall('biofluid')
+				
+				biofluid_dict = get_biofluid_locations(biofluid_locations, compound_name)
+
+				#Add new entries of the same MW to that MW's dictionary
+				#entry
+				output_nested_dict[MW].update({hmdb_id: biofluid_dict})
+
+				#hmdb_properties = {hmdb_id: biofluid_info}
+
+			#output_nested_dict[MW] = {hmdb_id:biofluid_info}
+
+		element.clear()
+	log_file.close()
+	#Raise error if no hmdb matches were found
+	if not output_nested_dict:
+		raise ValueError('No hits were found in HMDB for the masses\
+						 you searched: %s' % masses)
+	return output_nested_dict
+			#output_nested_dict[MW] = 
+			#Get 			
+
+def get_biofluid_locations(biofluid_xml, compound_name):
 	'''
-	INPUT -	A BeautifulSoup object from an individual HMDB accession number's xml file
-	FUNCTION - gets the various biofluid locations from the xml
-	OUTPUT - {Name: compound_name, Urine: 0, Feces:1, Other_fluids: 'string'}
+	INPUT - xml of the <biofluid_locations> tag from hmdb 
+	FUNCTION - parse that xml and return the biofluids biofluids present 
+	OUTPUT - {Urine: (1/0), Feces: (1/0), Other Biofluids: ['string']}
 	'''
-	compound_name = hmdb_metabolite.find('name').contents[0]
-	#print compound_name
-	biofluids_present = {'Name': compound_name, 'Urine': 0, 'Feces': 0, 'Other Biofluids': 'None Listed'}
-	Other_fluids = []
-	#Get all the biofluid locations listed and run
-	#through them, looking especially for Urine and Feces
-	biofluid_locations = hmdb_metabolite.biofluid_locations.find_all('biofluid')
-	for biofluid in biofluid_locations:
-		#print biofluid.contents
-		if biofluid.contents[0] == 'Urine':
-			biofluids_present['Urine'] = 1
-			break
-		elif biofluid.contents[0] == 'Feces':
-			biofluids_present['Feces'] = 1
-			break
+	biofluid_info = {'Name': compound_name, 'Urine': 0, 'Feces': 0, 'Other Biofluids': []}
+	other_fluids = []
+	for fluid in biofluid_xml:
+		if fluid.text == 'Urine':
+			biofluid_info['Urine'] = 1
+		elif fluid.text == 'Feces':
+			biofluid_info['Feces'] = 1
 		else:
-			Other_fluids.append(biofluid.contents[0])
-	if len(Other_fluids) > 0:
-		#Turn list into string
-		fluids = ', '.join(Other_fluids)
-		biofluids_present['Other Biofluids'] = fluids
+			other_fluids.append(fluid.text)
+	#append the non-urine/feces biofluids
+	biofluid_info['Other Biofluids'] = ', '.join(other_fluids)
 
-	return biofluids_present
-
-def combine_info(biofluids, metlin_dataframe, isomers):
-	'''
-	INPUT - Dataframe of HMDB, Urine, feces, and mass info
-	FUNCTION - Merge the metlin mass, adduct, and dppm with
-		# isomers, Urine, Feces, and Other biofluid presence
-	OUTPUT - Pandas dataframe with [mass, adduct, ppm, Urine,
-		Feces, Other] as headers.
-	'''
-	#make the mass values indices for the dataframe
-	metlin_dataframe = metlin_dataframe.set_index('mass')
-	curated_df = pandas.concat([metlin_dataframe['adduct'],
-								metlin_dataframe['dppm']],
-								axis=1)
-	curated_df['Isomers'] = pandas.Series(index=curated_df.index)
-	#If isomer mass is equal to row mass, isomer category
-	# in dataframe = mass from isomers
-
-	#Add the number of isomers to dataframe
-	#print curated_df.index
-	curated_df = add_isomers_to_data(curated_df, isomers)
-	curated_df = curated_df.drop_duplicates()
-
-	#Testing to see how program reacts to two compounds with same mass
-	'''
-	fuck_shit_up = {'Other Biofluids': u'SHIT', 'Feces': 'crap', 'Urine': 'poo', 'Name': u'Trying to fuck it up'}
-	#print '\n fuck it up \n', fuck_shit_up
-	fuck_up_df = pandas.DataFrame(fuck_shit_up, index=[74.08439833])
-	#print fuck_up_df
-
-	shitty_biofluids = pandas.concat([biofluids, fuck_up_df], axis=0)
-	#print shitty_biofluids
-	'''
-	#Iterate through biofluids rows, extract mass. Iterate through 
-	#metlin info. if they're equal, add metlin stuff to biofluids
-	combined_df = pandas.DataFrame()
-
-	#row_id is the mass, row is the urine, feces, etc info.
-	for row_id, row in biofluids.iterrows():
-		#mass is the mass, met_row is the row info - isomers, adduct, etc
-		for mass, met_row in curated_df.iterrows():
-			#if the hmdb mass matches a mass from metlin
-			#(Which it should have to) Then add the metlin isomer
-			#and adduct information to the biofluid information
-			if row_id == mass:
-				concat_data = pandas.concat([row, met_row])
-				combined_df = pandas.concat([combined_df, concat_data], axis=1)
-	
-	nearly_final_output = combined_df.T.set_index('HMDB_ID')
-	#Get the list of columns
-	column_order_original = nearly_final_output.columns.tolist()
-	column_order_final = ['mass', 'dppm', 'adduct', 'Isomers', 'Name',
-				    'Urine', 'Feces', 'Other Biofluids']
-
-	final_output = nearly_final_output[column_order_final]
-	return final_output
-
-
-def add_isomers_to_data(metlin_data_curated, isomers_data):
-	'''Takes a main dataframe with an empty isomers, a dataframe 
-	containing hte number of isomers at each mass, and adds the 
-	isomer information to the main dataframe'''
-	for mass_value in metlin_data_curated.index:
-		#Add number of isomers at a given mass to each row
-		#representing that mass
-		for metlin_mass in isomers.index:
-			if mass_value == metlin_mass:
-				metlin_data_curated.loc[mass_value, 'Isomers'] = isomers[metlin_mass]
-	return metlin_data_curated
+	return biofluid_info
 
 def nested_dict_to_dataframe(nested_dict):
 	#print nested_dict
@@ -224,24 +155,77 @@ def nested_dict_to_dataframe(nested_dict):
 			#print output
 	return output
 
+def combine_hmdb_metlin(hmdb_df, metlin_df, MW_deviation_tolerance):
+	'''
+	duplicate_mass =pandas.DataFrame({'Feces': 'shit', 
+		'Urine':'piss', 'Other Biofluids': "you don't wanna know", 
+		'HMDB_ID': 'asdf', 'mass': 74.084398}, index=[74.08439833])
+	
+	duplicate_mass_test = pandas.concat([hmdb_df,  duplicate_mass], axis=0)
+	print duplicate_mass_test
+	'''
+	
+	combined_df = pandas.DataFrame()
+	for hmdb_mass, hmdb_row in hmdb_df.iterrows():
+		for metlin_mass, metlin_row in metlin_df.iterrows():
+			#if the masses in hmdb match a mass from metlin, which 
+			#they have to, add the metlin info to hmdb_info
+			if abs(hmdb_mass - metlin_mass) < MW_deviation_tolerance:
+				concat_row = pandas.concat([metlin_row, hmdb_row])
+				combined_df = pandas.concat([combined_df, concat_row], axis=1)
+
+	#transpose dataframe and set HMDB_ID as index
+
+	combined_df = combined_df.T
+	combined_df = combined_df.set_index('HMDB_ID')
+	column_order_final = ['inputmass', 'mass', 'dppm', 'adduct', 'Isomers', 'Name',
+				    'Urine', 'Feces', 'Other Biofluids']
+	final_df = combined_df[column_order_final]
 
 
+	#output_df = pandas.concat([metlin_df, duplicate_mass_test],axis=1)
+	return final_df
 
-'''
-hmdb_identified = search_HMDB(toy_masses, metabolites) 
-print 'Found HMDB info: ', hmdb_identified
+#This is the maximum difference between a the MW we are 
+#searching for and the MW we find in HMDB. 
+#We have to do this because we cannot compare 
+#floating-point integers via equality - 29.0 could 
+#be represented as 28.999999999 and 29.000000000
+MW_deviation_tolerance = 1e-9
+#import csv to pandas, with first row as the header
+metlin_data = pandas.read_csv(metlin_csv, header=0)
+#metlin_data = metlin_data.set_index('mass')
+#print '\n Metlin Data: \n %s' % metlin_data
+#Count number of repeat input mass values - the number of isomers
+isomers = metlin_data['mass'].value_counts()
+#print '\n Isomers \n%s' % isomers
+masses = isomers.index
+print 'Searching HMDB for %s distinct masses' % masses.size
+#print '\n Masses \n%s' % masses
 
-biofluids_nested_dict = get_hmdb_info(hmdb_identified)
-print '\n Biofluid_info as nested dict',biofluids_nested_dict
-biofluid_dataframe = nested_dict_to_dataframe(biofluids_nested_dict)
-print '\nBiofluid dataframe\n', biofluid_dataframe
-print biofluid_dataframe
+selected_metlin_data = pandas.concat([metlin_data['inputmass'],metlin_data['mass'],metlin_data['adduct'],
+									  metlin_data['dppm']],
+									  axis=1)
+#print '\nselected data:\n %s' % selected_metlin_data
 
-final_output = combine_info(biofluid_dataframe, metlin_data, isomers)
-print '\nfinal data to output to file\n', final_output
+#Remove duplicate masses and set row indices to mass values
+selected_metlin_data = selected_metlin_data.drop_duplicates().set_index('mass')
+selected_metlin_data['Isomers'] = isomers
+#print '\nSelected Data + Isomers:\n%s'% selected_metlin_data
 
-#output_file = open(output_file)
-final_output.to_csv(output_filename)
-'''
+#pandas.set_option('precision', 10)
+nested_dict = get_data_from_hmdb(hmdb_all_metabolites, 
+								 masses, MW_deviation_tolerance)
+#print '\n I found the following compounds in HMDB: \n %s' % nested_dict
+
+hmdb_df = nested_dict_to_dataframe(nested_dict)
+#print '\nHMDB_datafame: \n %s' % hmdb_df 
 
 
+combined_df = combine_hmdb_metlin(hmdb_df, selected_metlin_data, MW_deviation_tolerance)
+print '\nI found %s compounds in HMDB\n' % combined_df.shape[0]
+print 'Some metabolites may have been skipped. See the log file: log_parse_hmdb.txt\n'
+print 'Saved the output at %s' % output_filename
+
+
+combined_df.to_csv(output_filename, encoding='utf-8')
